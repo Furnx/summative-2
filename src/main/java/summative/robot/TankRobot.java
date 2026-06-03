20a82814cd2d5719682e4632075c366d278f99b6
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-03 21:58:34 +0200

    added tank robot class

---
+++ src/main/java/summative/robot/TankRobot.java
@@ -0,0 +1,10 @@
+package summative.robot;
+
+public class TankRobot extends Robot {
+
+    // maxShields = 5, maxShots = 3 (heavy armour, fewer shots)
+
+    // TODO: implement constructor
+
+    // TODO: implement maxShields() and maxShots()
+}

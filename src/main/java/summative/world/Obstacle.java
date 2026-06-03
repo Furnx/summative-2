8ef317348dbf76a382ab945b92838beb78b81ffe
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-03 22:01:14 +0200

    added obstacle class

---
+++ src/main/java/summative/world/Obstacle.java
@@ -0,0 +1,14 @@
+package summative.world;
+
+public class Obstacle {
+
+    // TODO: declare private final fields topLeft and bottomRight (Position)
+
+    // TODO: implement constructor
+
+    // TODO: implement getters topLeft() and bottomRight()
+
+    // TODO: implement blocks(Position p)
+
+    // TODO: override toString()
+}

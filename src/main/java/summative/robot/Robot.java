212ab893c8a871e0213549304b8245b7b7428d19
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-03 21:53:42 +0200

    added robot class

---
+++ src/main/java/summative/robot/Robot.java
@@ -0,0 +1,26 @@
+package summative.robot;
+
+public abstract class Robot {
+
+    public enum RobotStatus {
+        NORMAL,
+        REPAIR,
+        RELOAD,
+        DEAD
+    }
+
+    // TODO: declare private fields here
+
+    // TODO: implement constructor(String name, int maxShields, int maxShots)
+
+    // TODO: implement getters
+
+    // TODO: implement updatePosition, updateDirection, updateStatus
+
+    // TODO: implement takeHit()
+
+    // TODO: declare abstract int maxShields()
+    // TODO: declare abstract int maxShots()
+
+    // TODO: override toString()
+}

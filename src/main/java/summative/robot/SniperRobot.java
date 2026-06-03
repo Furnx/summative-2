270303507d888c800bfe9aa6e690369a9d8e9588
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-03 21:56:31 +0200

    added sniper robot class

---
+++ src/main/java/summative/robot/SniperRobot.java
@@ -0,0 +1,12 @@
+package summative.robot;
+
+import java.awt.*;
+
+public class SniperRobot extends Robot {
+
+    // maxShields = 1, maxShots = 1 (long range, fragile)
+
+    // TODO: implement constructor
+
+    // TODO: implement maxShields() and maxShots()
+}

89989682c41966d6c04052c3b50061e87c6344df
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-03 22:08:56 +0200

    added world class

---
+++ src/main/java/summative/world/World.java
@@ -0,0 +1,25 @@
+package summative.world;
+
+import summative.robot.Robot;
+
+import java.util.List;
+import java.util.Map;
+
+public class World {
+
+    // TODO: declare private final fields width, height (int)
+    // TODO: declare private field obstacles (List<Obstacle>)
+    // TODO: declare private field robots (Map<String, Robot>)
+
+    // TODO: implement constructor(int width, int height, List<Obstacle> obstacles)
+
+    // TODO: implement width(), height(), obstacles(), robots()
+
+    // TODO: implement isInBounds(Position p)
+
+    // TODO: implement isBlocked(Position p)
+
+    // TODO: implement addRobot(Robot robot, Position startPosition)
+
+    // TODO: implement listRobots()
+}
